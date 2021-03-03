f9f01e81656c2ebe086c9af18473c4b690156574
annawolff02 <78774828+annawolff02@users.noreply.github.com>
2021-03-02 21:08:43 -0600

    moved draw square to render and created draw triangle in render

--- Assignment03.js
+++ Assignment03.js
@@ -95,17 +95,7 @@ window.onload = function init()
     gl.enableVertexAttribArray(colorLoc);
 
 
-    // Load the data into the GPU
-
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
-
-    // Associate out shader variables with our data bufferData
-
-    var positionLoc = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
+    
 
     //set up uniform variable and establish shaders
     program = initShaders(gl, "vertex-shader", "fragment-shader");
@@ -146,6 +136,35 @@ function render() {
 
     //draw square
     gl.useProgram(program);
+
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data bufferData
+    var positionLoc = gl.getAttribLocation(program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    //draw it!
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    //draw triagnle, switch to triangle shaders
+    gl.useProgram(programTriangle);
+
+    //load the data
+    var bufferId2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId2);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(verticesTriangle), gl.STATIC_DRAW);
+
+    //associate shader variables with out data bufferData
+    var positionLoc2 = gl.getAttribLocation(programTriangle, "aPosition");
+    gl.vertexAttribPointer(positionLoc2, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc2);
+
+    //draw it!
+    gl.drawArrays(gl.TRIANGLES, 0, verticesTriangle.length);
 
     //change speed for slider
     if (direction == true) {
